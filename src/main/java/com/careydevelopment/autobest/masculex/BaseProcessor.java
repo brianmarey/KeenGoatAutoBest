d132992220de1f4437c07218e0b25df52e3c78d7
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-11-24 18:30:03 -0500

    added new site

---
+++ src/main/java/com/careydevelopment/autobest/masculex/BaseProcessor.java
@@ -0,0 +1,223 @@
+package com.careydevelopment.autobest.masculex;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import com.careydevelopment.autobest.domain.Post;
+import com.careydevelopment.autobest.domain.Product;
+import com.careydevelopment.autobest.util.AmazonDocumentParser;
+import com.careydevelopment.autobest.util.BrandParser;
+import com.careydevelopment.autobest.util.IntroHelper;
+import com.careydevelopment.autobest.util.Sanitizer;
+import com.careydevelopment.autobest.util.SellerUrlHelper;
+import com.careydevelopment.autobest.util.SlugHelper;
+
+public abstract class BaseProcessor {
+	
+	private Connection connect = null;
+	private Properties props = null;
+	private int contextId;
+	
+	protected String[][] categories = {{}};
+	protected String[] nodes = {};
+	protected String[] brandNames = {};
+	protected String contextName;
+	
+	protected void go() {		
+		try {
+			props = getProperties();
+			String connectionString = props.getProperty("jdbc.connection");	
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			connect = DriverManager.getConnection(connectionString);
+			
+			fetchContext();
+			
+			for (int i=0; i<categories.length; i++) {
+				try {
+					processCategory(i);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (connect != null) {
+				try {
+					connect.close();
+				} catch (Exception e) {}
+			}		
+			
+		}
+	}
+	
+	
+	private void fetchContext() {
+		try (PreparedStatement ps = connect.prepareStatement("select id from context where name=?")) {
+			ps.setString(1, contextName);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if (rs.next()) {
+				contextId = rs.getInt(1);
+			} else {
+				throw new RuntimeException("Can't find context!");
+			}
+			
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	private Properties getProperties() {
+		Properties prop = new Properties();
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		
+		try (InputStream in = loader.getResourceAsStream("application.properties")) {
+			prop.load(in);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+				
+		return prop;
+	}
+	
+	
+	private void processCategory(int index) {
+		String category = categories[index][0];
+		String node = nodes[index];
+
+		List<String> brands = BrandParser.parse(brandNames[index]);
+		List<String> titles = new ArrayList<String>();
+
+		for (String brand : brands) {
+			try {
+				Post post = new Post();
+				
+				String title = "The 20 Best " + brand + " " + category + " for 2018";
+				title = Sanitizer.sanitize(title);
+							
+				if (notFound(titles, title)) {
+					titles.add(title);
+					String intro = IntroHelper.getIntro(brand, categories[index][1]); 
+					String slug = SlugHelper.getSlug(title);
+					
+					post.setDate(new Date());
+					post.setIntro(intro);
+					post.setSlug(slug);
+					post.setTitle(title);
+					
+					addListicle(post, node, brand);
+					Thread.sleep(1000);
+				}	
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	
+	private void addListicle(Post post, String node, String brand) {
+		List<Product> allProducts = new ArrayList<Product>();
+		
+		for (int i=1;i<=2;i++) {
+			SellerUrlHelper urlHelper = new SellerUrlHelper(brand,node,i,null);
+			String url = urlHelper.getUrl();
+
+			AmazonDocumentParser parser = new AmazonDocumentParser(url);
+			List<Product> products = parser.getProducts();
+			allProducts.addAll(products);
+		}
+		
+		if (allProducts != null && allProducts.size() > 17) {
+			addPost(post);
+			if (post.getId() > 0) addList(post, allProducts);
+			else throw new RuntimeException ("Couldn't find ID!");			
+		}
+	}
+	
+	
+	private void addList(Post post, List<Product> products) {
+		for (Product product : products) {
+			try {
+				product.setPostId(post.getId());
+				persistProduct(product);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	private void persistProduct(Product product) {
+		try (PreparedStatement ps = connect.prepareStatement("insert into product (title, description, post_id, link, imageUrl, highestPrice, lowestPrice) values (?,?,?,?,?,?,?)")) {
+			ps.setString(1, product.getTitle());
+			ps.setString(2, product.getDescription());
+			ps.setInt(3, product.getPostId());
+			ps.setString(4, product.getLink());
+			ps.setString(5, product.getImageUrl());
+			ps.setString(6,  product.getHighestPrice());
+			ps.setString(7, product.getLowestPrice());
+			
+			ps.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private void addPost(Post post) {
+		int id = 0;
+		
+		try (PreparedStatement ps = connect.prepareStatement("insert into post (slug,intro,date,title,context_id) values (?,?,?,?,?)")) {
+			ps.setString(1, post.getSlug());
+			ps.setString(2, post.getIntro());
+			ps.setDate(3, new java.sql.Date(post.getDate().getTime()));
+			ps.setString(4, post.getTitle());
+			ps.setInt(5, contextId);
+			
+			ps.execute();			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try (PreparedStatement ps = connect.prepareStatement("select id from post where slug = ?")) {
+			ps.setString(1, post.getSlug());
+			
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				id = rs.getInt(1);
+				post.setId(id);
+			}			
+			
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private boolean notFound(List<String> list, String str) {
+		boolean notFound = true;
+		
+		for (String item : list) {
+			if (item.toLowerCase().trim().equals(str.toLowerCase().trim())) {
+				notFound = false;
+				break;
+			} 
+		}
+		
+		return notFound;
+	}
+
+}
--- src/main/java/com/careydevelopment/autobest/masculex/ProcessMasculex.java
+++ src/main/java/com/careydevelopment/autobest/masculex/ProcessMasculex.java
@@ -1,32 +1,8 @@
 package com.careydevelopment.autobest.masculex;
 
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Properties;
-
-import com.careydevelopment.autobest.domain.Context;
-import com.careydevelopment.autobest.domain.Post;
-import com.careydevelopment.autobest.domain.Product;
-import com.careydevelopment.autobest.util.AmazonDocumentParser;
-import com.careydevelopment.autobest.util.BrandParser;
-import com.careydevelopment.autobest.util.IntroHelper;
-import com.careydevelopment.autobest.util.Sanitizer;
-import com.careydevelopment.autobest.util.SellerUrlHelper;
-import com.careydevelopment.autobest.util.SlugHelper;
-
-public class ProcessMasculex {
-
-	private Connection connect = null;
-	private Properties props = null;
-	private int contextId;
+public class ProcessMasculex extends BaseProcessor {
 		
-	private static final String[][] CATEGORIES = {{"Men's Polo Shirts","Men's Polo Shirt"},
+	protected String[][] categories = {{"Men's Polo Shirts","Men's Polo Shirt"},
 		{"Men's Button-Down Casual Shirts","Men's Button-Down Casual Shirt"},
 		{"Men's Dress Shirts","Men's Dress Shirt"},
 		{"Men's Pullover Sweaters","Men's Pullover Sweater"},
@@ -44,10 +20,10 @@ public class ProcessMasculex {
 		{"Men's Suits","Men's Suit"},
 		{"Men's Sport Coats","Men's Sport Coat"}};
 
-	private static final String[] NODES = {"1045640","1045630","1045626","2476516011","1045658","2476494011","2528780011","2476604011",
+	protected String[] nodes = {"1045640","1045630","1045626","2476516011","1045658","2476494011","2528780011","2476604011",
 		"1045564","2476497011","2476498011","2476500011","2476501011","2528724011","2528725011","1045686","1045694"};
 	
-	private static final String[] BRANDS = {"https://www.amazon.com/gp/search/other/ref=lp_1045640_sa_p_89?rh=n%3A7141123011%2Cn%3A7147441011%2Cn%3A1040658%2Cn%3A2476517011%2Cn%3A1045640&bbn=1045640&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1511013168",
+	protected String[] brandNames = {"https://www.amazon.com/gp/search/other/ref=lp_1045640_sa_p_89?rh=n%3A7141123011%2Cn%3A7147441011%2Cn%3A1040658%2Cn%3A2476517011%2Cn%3A1045640&bbn=1045640&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1511013168",
 		"https://www.amazon.com/gp/search/other/ref=lp_1045630_sa_p_89?rh=n%3A7141123011%2Cn%3A7147441011%2Cn%3A1040658%2Cn%3A2476517011%2Cn%3A1045630&bbn=1045630&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1511439580",
 		"https://www.amazon.com/gp/search/other/ref=lp_1045626_sa_p_89?rh=n%3A7141123011%2Cn%3A7147441011%2Cn%3A1040658%2Cn%3A2476517011%2Cn%3A1045626&bbn=1045626&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1511439864",
 		"https://www.amazon.com/gp/search/other/ref=lp_2476516011_sa_p_89?rh=n%3A7141123011%2Cn%3A7147441011%2Cn%3A1040658%2Cn%3A1044442%2Cn%3A2476516011&bbn=2476516011&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1511439922",
@@ -65,200 +41,10 @@ public class ProcessMasculex {
 		"https://www.amazon.com/gp/search/other/ref=lp_1045686_sa_p_89?rh=n%3A7141123011%2Cn%3A7147441011%2Cn%3A1040658%2Cn%3A1045684%2Cn%3A1045686&bbn=1045686&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1511442009",
 		"https://www.amazon.com/gp/search/other/ref=lp_1045694_sa_p_89?rh=n%3A7141123011%2Cn%3A7147441011%2Cn%3A1040658%2Cn%3A1045684%2Cn%3A1045694&bbn=1045694&pickerToList=lbr_brands_browse-bin&ie=UTF8&qid=1511442100"};
 	
-	private String CONTEXT_NAME = "masculex";
+	protected String contextName = "masculex";
 	
 	public static void main(String[] args) {
 		ProcessMasculex pm = new ProcessMasculex();
 		pm.go();
-	}
-	
-	
-	private void go() {		
-		try {
-			props = getProperties();
-			String connectionString = props.getProperty("jdbc.connection");	
-			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-			connect = DriverManager.getConnection(connectionString);
-			
-			fetchContext();
-			
-			for (int i=0; i<CATEGORIES.length; i++) {
-				try {
-					processCategory(i);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (connect != null) {
-				try {
-					connect.close();
-				} catch (Exception e) {}
-			}
-		}
-	}
-	
-	
-	private void fetchContext() {
-		try (PreparedStatement ps = connect.prepareStatement("select id from context where name=?")) {
-			ps.setString(1, CONTEXT_NAME);
-			
-			ResultSet rs = ps.executeQuery();
-			
-			if (rs.next()) {
-				contextId = rs.getInt(1);
-			} else {
-				throw new RuntimeException("Can't find context!");
-			}
-			
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	
-	private Properties getProperties() {
-		Properties prop = new Properties();
-		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		
-		try (InputStream in = loader.getResourceAsStream("application.properties")) {
-			prop.load(in);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-				
-		return prop;
-	}
-	
-	
-	private void processCategory(int index) {
-		String category = CATEGORIES[index][0];
-		String node = NODES[index];
-
-		List<String> brands = BrandParser.parse(BRANDS[index]);
-		List<String> titles = new ArrayList<String>();
-
-		for (String brand : brands) {
-			try {
-				Post post = new Post();
-				
-				String title = "The 20 Best " + brand + " " + category + " for 2018";
-				title = Sanitizer.sanitize(title);
-							
-				if (notFound(titles, title)) {
-					titles.add(title);
-					String intro = IntroHelper.getIntro(brand, CATEGORIES[index][1]); 
-					String slug = SlugHelper.getSlug(title);
-					
-					post.setDate(new Date());
-					post.setIntro(intro);
-					post.setSlug(slug);
-					post.setTitle(title);
-					
-					addListicle(post, node, brand);
-					Thread.sleep(1000);
-				}	
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	
-	private void addListicle(Post post, String node, String brand) {
-		List<Product> allProducts = new ArrayList<Product>();
-		
-		for (int i=1;i<=2;i++) {
-			SellerUrlHelper urlHelper = new SellerUrlHelper(brand,node,i,null);
-			String url = urlHelper.getUrl();
-
-			AmazonDocumentParser parser = new AmazonDocumentParser(url);
-			List<Product> products = parser.getProducts();
-			allProducts.addAll(products);
-		}
-		
-		if (allProducts != null && allProducts.size() > 17) {
-			addPost(post);
-			if (post.getId() > 0) addList(post, allProducts);
-			else throw new RuntimeException ("Couldn't find ID!");			
-		}
-	}
-	
-	
-	private void addList(Post post, List<Product> products) {
-		for (Product product : products) {
-			try {
-				product.setPostId(post.getId());
-				persistProduct(product);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	
-	private void persistProduct(Product product) {
-		try (PreparedStatement ps = connect.prepareStatement("insert into product (title, description, post_id, link, imageUrl, highestPrice, lowestPrice) values (?,?,?,?,?,?,?)")) {
-			ps.setString(1, product.getTitle());
-			ps.setString(2, product.getDescription());
-			ps.setInt(3, product.getPostId());
-			ps.setString(4, product.getLink());
-			ps.setString(5, product.getImageUrl());
-			ps.setString(6,  product.getHighestPrice());
-			ps.setString(7, product.getLowestPrice());
-			
-			ps.execute();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	private void addPost(Post post) {
-		int id = 0;
-		
-		try (PreparedStatement ps = connect.prepareStatement("insert into post (slug,intro,date,title,context_id) values (?,?,?,?,?)")) {
-			ps.setString(1, post.getSlug());
-			ps.setString(2, post.getIntro());
-			ps.setDate(3, new java.sql.Date(post.getDate().getTime()));
-			ps.setString(4, post.getTitle());
-			ps.setInt(5, contextId);
-			
-			ps.execute();			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		try (PreparedStatement ps = connect.prepareStatement("select id from post where slug = ?")) {
-			ps.setString(1, post.getSlug());
-			
-			ResultSet rs = ps.executeQuery();
-			if (rs.next()) {
-				id = rs.getInt(1);
-				post.setId(id);
-			}			
-			
-			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	private boolean notFound(List<String> list, String str) {
-		boolean notFound = true;
-		
-		for (String item : list) {
-			if (item.toLowerCase().trim().equals(str.toLowerCase().trim())) {
-				notFound = false;
-				break;
-			} 
-		}
-		
-		return notFound;
 	}
 }
